e96f13c0955198877a55ba1d3e4b2d36e03d0f04
Benjamin Sergent <benjamin.sergent@daqri.com>
2015-10-16 11:08:10 -0700

    Added -y flags

--- dependancies/linux_setup_dev_machine.sh
+++ dependancies/linux_setup_dev_machine.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
 #Repositories
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository ppa:george-edison55/cmake-3.x
-sudo add-apt-repository ppa:mystic-mirage/pycharm
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+sudo add-apt-repository -y ppa:mystic-mirage/pycharm
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 
 #Get list and update
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 #dev tools
-sudo apt-get install nautilus-open-terminal
-sudo apt-get install build-essential
-sudo apt-get install software-properties-common
-sudo apt-get install gcc g++ cpp
-sudo apt-get install clang-3.5
-sudo apt-get install clang++-3.5
-sudo apt-get install cmake cmake-gui
+sudo apt-get -y install nautilus-open-terminal
+sudo apt-get -y install build-essential
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install gcc g++ cpp
+sudo apt-get -y install clang-3.5
+sudo apt-get -y install clang++-3.5
+sudo apt-get -y install cmake cmake-gui
 
 #Dev Other 
-sudo apt-get install liblog4cxx10 liblog4cxx10-dev
-sudo apt-get install gource
+sudo apt-get -y install liblog4cxx10 liblog4cxx10-dev
+sudo apt-get -y install gource
 
 #Graphics and CV
-sudo apt-get install libglfw-dev
-sudo apt-get install libglew-dev
-sudo apt-get install libopencv-dev
-sudo apt-get install nvidia-opencl-dev
-sudo apt-get install binutils-gold
-sudo apt-get install freeglut3 freeglut3-dev
+sudo apt-get -y install libglfw-dev
+sudo apt-get -y install libglew-dev
+sudo apt-get -y install libopencv-dev
+sudo apt-get -y install nvidia-opencl-dev
+sudo apt-get -y install binutils-gold
+sudo apt-get -y install freeglut3 freeglut3-dev
 sudo sh install_glfw.sh
 sudo sh install_png16.sh
 
 #IDEs and Text Editors
-sudo apt-get install netbeans
-sudo apt-get install pycharm
-sudo apt-get install sublime-text-installer
+sudo apt-get -y install netbeans
+sudo apt-get -y install pycharm
+sudo apt-get -y install sublime-text-installer
 
 #Media Editing
-sudo apt-get install handbrake
-sudo apt-get install gimp gimp-data gimp-plugin-registry gimp-data-extras
-sudo apt-get install audacity
+sudo apt-get -y install handbrake
+sudo apt-get -y install gimp gimp-data gimp-plugin-registry gimp-data-extras
+sudo apt-get -y install audacity
 
 #Entertainment
-sudo apt-get install steam
-sudo apt-get install vlc browser-plugin-vlc
+sudo apt-get -y install steam
+sudo apt-get -y install vlc browser-plugin-vlc
