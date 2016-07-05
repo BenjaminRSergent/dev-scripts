836084b630a466fcd0d404b454f0ebeedb7f8032
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-07-05 13:48:19 -0700

    Added more installation scripts

---
+++ full_setup.sh
@@ -0,0 +1,8 @@
+cd dependancies
+sudo ./linux_setup_dev_machine.sh
+
+cd ../scripts
+./install_to_scripts.sh
+
+cd ../setup/
+./append.sh
---
+++ scripts/install_to_scripts.sh
@@ -0,0 +1,2 @@
+mkdir ~/scripts > /dev/null
+cp */* ~/scripts/
