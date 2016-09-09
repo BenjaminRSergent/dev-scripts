ceac8d281824f3bc6fbef530e9dbad962c949444
Apar Suri <apar.suri@daqri.com>
2016-09-08 18:06:40 -0700

    Improved graphing script

--- scripts/data_processing/graph_columns.py
+++ scripts/data_processing/graph_columns.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 from itertools import islice
 
 
 if len(sys.argv) < 6:
-    print("usage: graph file columns_per_row x_column y_column x_label y_label")
+    print("usage: graph file title columns_per_row x_column_name x_column y_column_name y_column x_label y_label")
 
 datafile = open(sys.argv[1])
-columnsperrow = int(sys.argv[2])
-columnx = int(sys.argv[3])
-columny = int(sys.argv[4])
+title = sys.argv[2]
+columnsperrow = int(sys.argv[3])
 
-labelx = sys.argv[5]
-labely = sys.argv[6]
+legandx = sys.argv[4]
+columnx = int(sys.argv[5])
+
+legandy = sys.argv[6]
+columny = int(sys.argv[7])
+
+labelx = sys.argv[8]
+labely = sys.argv[9]
 
 datalines = [x.split() for x in datafile.readlines()]
 datafile.close()
 
 data = [float(element) for sublist in datalines for element in sublist]
 
-print(data)
-
 datax = data[columnx::columnsperrow]
 datay = data[columny::columnsperrow]
 
-plt.plot(datax,datay)
+frames = np.arange(len(datalines))
+
+plt.gca().set_color_cycle(['red', 'green', 'blue', 'yellow'])
+
+plt.plot(frames, datax, label=legandx)
+plt.plot(frames, datay, label=legandy)
 plt.xlabel(labelx)
 plt.ylabel(labely)
+plt.title(title)
+
+plt.legend(loc='upper left')
 plt.show()
