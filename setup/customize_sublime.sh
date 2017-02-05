323a1d877b2a24af06dfc2a8328d090212e2c06b
BenjaminRSergent <benjamin.sergent@daqri.com>
2017-02-04 21:16:35 -0800

    Fixed sublime customize permissions

--- setup/customize_sublime.sh
+++ setup/customize_sublime.sh
@@ -1,3 +1,4 @@
-sudo cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+mkdir -p Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
+cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 sudo sed -i -- 's/gedit/sublime-text/g' /usr/share/applications/defaults.list
 
