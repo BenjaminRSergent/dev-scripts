9af283af5bd127188ca97b6ec36be5465a68effe
Benjamin Sergent <benjamin.sergent@daqri.com>
2017-03-29 10:35:56 -0700

    Added ccache, clang-format and clang-tidy

--- dependancies/linux_setup_dev_machine.sh
+++ dependancies/linux_setup_dev_machine.sh
@@ -20,7 +20,7 @@ sudo apt -y install nautilus-open-terminal
 sudo apt -y install build-essential
 sudo apt -y install software-properties-common
 sudo apt -y install gcc g++ cpp
-sudo apt -y install clang clang-3.8 libclang1-3.8 libllvm3.8 lldb-3.8 llvm-3.8 llvm-3.8-runtime 
+sudo apt -y install clang clang-3.8 libclang1-3.8 libllvm3.8 lldb-3.8 llvm-3.8 llvm-3.8-runtime
 sudo apt -y install cmake cmake-gui
 sudo apt -y install libc++1
 sudo apt -y install multiarch-support
@@ -30,11 +30,14 @@ sudo apt -y install libc++abi1
 sudo apt -y install libtbb2
 sudo apt -y install libiomp5
 sudo apt -y install libc++-helpers
-sudo apt -y install openssh-server 
-
-#Dev Other 
+sudo apt -y install openssh-server
+sudo apt -y install ccache
 sudo apt -y install valgrind
 sudo apt -y install massif-visualizer
+sudo apt -y install clang-format
+sudo apt -y install clang-tidy
+
+#Dev Other
 sudo apt -y install liblog4cxx10 liblog4cxx10-dev
 sudo apt -y install libgoogle-glog-dev
 sudo apt -y install gource
@@ -42,6 +45,7 @@ sudo apt -y install zlib1g
 sudo apt -y install libusb-1.0-0
 sudo apt -y install lm-sensors
 sudo apt -y install rapidjson-dev
+
 
 #Graphics and CV
 sudo apt -y install libglfw3-dev
@@ -54,8 +58,8 @@ sudo apt -y install freeglut3 freeglut3-dev
 sudo apt -y install libassimp-dev
 sudo apt -y install opencl-headers
 sudo apt -y install libpng12-dev
-sudo apt -y install libavcodec-dev 
-sudo apt -y install libavformat-dev 
+sudo apt -y install libavcodec-dev
+sudo apt -y install libavformat-dev
 sudo apt -y install libswscale-dev
 
 #Python libs
@@ -64,7 +68,7 @@ sudo apt -y install python-numpy
 sudo apt -y install python-scipy
 sudo apt -y install python-pandas
 sudo apt -y install python-matplotlib
-sudo apt -y install python-pygraph 
+sudo apt -y install python-pygraph
 sudo apt -y install python-pygraphviz
 sudo apt -y install python-pygame
 
